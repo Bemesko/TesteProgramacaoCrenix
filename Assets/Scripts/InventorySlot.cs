370c12ea98d72e3fca8c8f86fd150eb1910ca0bf
Bernardo Mesko <bemesko@gmail.com>
2021-02-23 21:02:03 -0300

    Sistema de Drag e Drop do Tutorial

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    private RectTransform _spriteTransform;
+
+    private void Awake()
+    {
+        _spriteTransform = GetComponentInChildren<RectTransform>();
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Dropped");
+        if (eventData.pointerDrag != null)
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = _spriteTransform.anchoredPosition;
+    }
+}

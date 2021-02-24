f1daa6b8cc0021a7af48338d7fa8cb9fb1ae0e4d
Bernardo Mesko <bemesko@gmail.com>
2021-02-23 22:11:44 -0300

    Botão de Reset

---
+++ Assets/Scripts/DragAndDropManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropManager : MonoBehaviour
+{
+    public static DragAndDropManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse is down");
+    }
+}

80c95b2b09de7cbe1247deddfb36104e43cb93e4
Bernardo Mesko <bemesko@gmail.com>
2021-02-23 22:43:56 -0300

    Preparativos pra converter coordenadas de world para camera

--- Assets/Scripts/DragAndDropManager.cs
+++ Assets/Scripts/DragAndDropManager.cs
@@ -23,4 +23,28 @@ public class DragAndDropManager : MonoBehaviour
     {
         Debug.Log("Mouse is down");
     }
+
+    /* Código que converte world para canvas
+
+        //this is your object that you want to have the UI element hovering over
+        GameObject WorldObject;
+
+        //this is the ui element
+        RectTransform UI_Element;
+
+        //first you need the RectTransform component of your canvas
+        RectTransform CanvasRect = Canvas.GetComponent<RectTransform>();
+
+        //then you calculate the position of the UI element
+        //0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
+
+        Vector2 ViewportPosition = Cam.WorldToViewportPoint(WorldObject.transform.position);
+        Vector2 WorldObject_ScreenPosition = new Vector2(
+        ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+        ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+        //now you can set the position of the ui element
+        UI_Element.anchoredPosition=WorldObject_ScreenPosition;
+
+        */
 }

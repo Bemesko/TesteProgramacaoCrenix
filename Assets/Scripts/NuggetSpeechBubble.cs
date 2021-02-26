d71fa000356ba7d2e2f20bb46d3c4ba508d5e1a5
Bernardo Mesko <bemesko@gmail.com>
2021-02-26 11:53:36 -0300

    Fala do bichinho muda

---
+++ Assets/Scripts/NuggetSpeechBubble.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NuggetSpeechBubble : MonoBehaviour
+{
+    private TextMeshProUGUI _speechText;
+
+    private void Awake()
+    {
+        _speechText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        GearPlacementManager.OnSlotsFilled += ChangeText;
+    }
+
+    private void OnDisable()
+    {
+        GearPlacementManager.OnSlotsFilled -= ChangeText;
+    }
+
+    void ChangeText(bool isSlotsFilled)
+    {
+        Debug.Log("Change Text");
+        if (isSlotsFilled)
+        {
+            _speechText.SetText("Yay, parabéns. Task concluída!");
+        }
+        else
+        {
+            _speechText.SetText("Encaixe as engrenagens em qualquer ordem");
+        }
+    }
+}
